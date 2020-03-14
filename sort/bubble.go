39bbe7ef9e21483716e291a004e9b250e34df5cf
MingTaoChen <mail@mittacy.com>
2020-03-14 17:04:16 +0800

    复习排序算法

---
+++ sort/bubble.go
@@ -0,0 +1,25 @@
+package sort
+
+/*
+时间复杂度：O(N^2)
+ */
+func Bubbll(arr []int) {
+	length := len(arr)
+	if length <= 1 {
+		return
+	}
+	for i := 0; i < length; i++ {
+		// 提前退出冒泡循环的标志位
+		flag := false
+		for j := 0; j < length - 1 - i; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+				flag = true
+			}
+		}
+		if (!flag) {
+			// 满足条件说明本循环一次也没发生交换，说明数组已经排列好了
+			break
+		}
+	}
+}

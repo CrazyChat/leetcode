02c4e69a9a973accda9f53ba66cd01c13d164dda
MingTaoChen <mail@mittacy.com>
2020-04-08 18:11:48 +0800

    CountDigitOne

--- array/countDigitOne.go
+++ array/countDigitOne.go
@@ -1,5 +1,10 @@
 package array
 
+import (
+	"math"
+	"strconv"
+)
+
 /*
 输入一个整数 n ，求1～n这n个整数的十进制表示中1出现的次数。
 例如，输入12，1～12这些整数中包含1 的数字有1、10、11和12，1一共出现了5次。
@@ -11,17 +16,35 @@ package array
 链接：https://leetcode-cn.com/problems/1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof
  */
 
+//func CountDigitOne(n int) int {
+//	count, pow := 0, 1
+//	for n >= pow {
+//		count += n / (pow * 10) * pow
+//		if incr := n%(pow*10) - pow + 1; incr > pow {
+//			count += pow
+//		} else if incr > 0 {
+//			count += incr
+//		}
+//		pow *= 10
+//	}
+//	return count
+//}
+
 func CountDigitOne(n int) int {
-	count, pow := 0, 1
-	for n >= pow {
-		count += n / (pow * 10) * pow
-		if incr := n%(pow*10) - pow + 1; incr > pow {
-			count += pow
-		} else if incr > 0 {
-			count += incr
-		}
-		pow *= 10
-	}
-	return count
+	return f(n)
 }
 
+func f(n int) int {
+	if n <= 0 {
+		return 0
+	}
+	s := strconv.Itoa(n)
+	high := int(s[0] - '0')
+	pow := int(math.Pow10(len(s)-1))
+	last := n - high * pow
+	if high == 1 {
+		return f(pow-1) + last + 1 + f(last)
+	} else {
+		return pow + high * f(pow-1) + f(last)
+	}
+}
--- main.go
+++ main.go
@@ -2,10 +2,104 @@ package main
 
 import (
 	"fmt"
-	"github.com/crazychat/leetcode/array"
+	"math"
+	"time"
 )
 
+
 func main() {
-	nums := []int{1,3,5,4,2}
-	fmt.Println(array.NextGreaterElements(nums))
+	a := "mitcmusufhgr"
+	b := "mtacnusguhgr"
+	now := time.Now()
+	LwstBT(a, b)
+	fmt.Printf("%d, ", minDist)
+	fmt.Println("耗时: ", time.Since(now))
+	now = time.Now()
+	fmt.Printf("%d, ", LwstBTDB(a, b))
+	fmt.Println("耗时: ", time.Since(now))
+}
+
+func LwstBTDB(a, b string) int {
+	// 创建动态矩阵
+	minDist := make([][]int, len(a))
+	for i := range a {
+		minDist[i] = make([]int, len(b))
+	}
+	// 初始化第一行第一列
+	for j := 0; j < len(minDist[0]); j++ {
+		// 第一行
+		if a[0] == b[j] {
+			minDist[0][j] = j
+		} else if j != 0 {
+			minDist[0][j] = minDist[0][j-1]+1
+		} else {
+			minDist[0][j] = 1
+		}
+	}
+	// 第一列
+	for i := 0; i < len(minDist); i++ {
+		// 第一列
+		if b[0] == a[i] {
+			minDist[i][0] = i
+		} else if i != 0 {
+			minDist[i][0] = minDist[i-1][0] + 1
+		} else {
+			minDist[i][0] = 1
+		}
+	}
+	// 从第一行开始到结束填表
+	for i := 1; i < len(minDist); i++ {
+		for j := 1; j < len(minDist[0]); j++ {
+			if a[i] == b[j] {
+				minDist[i][j] = min(minDist[i][j-1]+1, minDist[i-1][j]+1, minDist[i-1][j-1])
+			} else {
+				minDist[i][j] = min(minDist[i][j-1]+1, minDist[i-1][j]+1, minDist[i-1][j-1]+1)
+			}
+		}
+	}
+	return minDist[len(minDist)-1][len(minDist[0])-1]
+}
+
+func min(x, y, z int) int {
+	if x > y {
+		x = y
+	}
+	if x > z {
+		return z
+	}
+	return x
+}
+
+var minDist =  math.MaxInt64 // 存储结果
+
+func LwstBT(a, b string) {
+	minDist = math.MaxInt64
+	f(a, b, 0, 0, 0)
+}
+
+func f(a, b string, i, j int, edit int) {
+	// 判断是否结束
+	if i == len(a) || j == len(b) {
+		// 删除i剩下的字符
+		if i < len(a) {
+			edit += len(a)-i
+		}
+		if j < len(b) {
+			edit += len(b)-j
+		}
+		if edit < minDist {
+			minDist = edit
+		}
+		return
+	}
+	if a[i] == b[j] { // 相等不用增加
+		f(a, b, i+1, j+1, edit)
+	} else {
+		// 删除a[i]或者b[j]前添加一个字符
+		f(a, b, i+1, j, edit+1)
+		// 删除b[j]或者a[i]前添加一个字符
+		f(a, b, i, j+1, edit+1)
+		// 将a[i]和b[j]替换为相同字符
+		f(a, b, i+1, j+1, edit+1)
+	}
 }

30d6570ae6c0e972db762e57f941166f5db9843d
MingTaoChen <mail@mittacy.com>
2020-04-11 12:11:04 +0800

    栈

--- main.go
+++ main.go
@@ -1,104 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/crazychat/leetcode/structure"
+)
 
 func main() {
-	T := 0	// T 组测试数据
-	// 每次更新
-	n, m, c := 0, 0, 0	// n*m 方格大小，c种颜色
-	// 赋值
-	_, _ = fmt.Scanf("%d", &T)
-	for {
-		n, _ = fmt.Scanf("%d %d %d", &n, &m, &c)
-		if n == 0 {
-			break
-		}
-		num := make([]int, c)	// 染料的数量
-		// 矩阵
-		trc := make([][]int, n)
-		for i := range trc {
-			trc[i] = make([]int, m)
-		}
-		// 填充
-		for i := 0; i < len(trc); i++ {
-			for j := 0; j < len(trc); j++ {
-				// 有染料
-
-				// 无染料，退出
-
-				fmt.Println("NO")
-				return
-			}
-		}
+	stack := structure.NewLinkStack()
+	for i := 0; i < 10; i++ {
+		stack.Push(i)
 	}
 
+	for !stack.IsEmpty() {
+		value, _ := stack.Pop()
+		fmt.Printf("%d ", value)
+	}
+	value, err := stack.Top()
+	if err != nil {
+		fmt.Println()
+		fmt.Println(err)
+	} else {
+		fmt.Println(value)
+	}
 }
-
-
-
-
-//func main() {
-//	n := 0
-//	// 赋值
-//	_, _ = fmt.Scanf("%d", &n)
-//	house := []int{}
-//	housey := 0
-//	for i := 0; i < n; i++ {
-//		_, _ = fmt.Scanf("%d %d", &house[i], housey)
-//	}
-//	// 动态规划表, arr[0][j] = house[j], arr[i][j] = house[j]
-//	arr := make([][]int, n)
-//	// 初始化第一行和第一列
-//	arr[0][0] = 0
-//	for j := 1; j < len(arr[0]); j++ {
-//		arr[0][j] =  f(house[j], house[0]) + arr[0][j-1]
-//	}
-//	for i := 1; i < len(arr); i++ {
-//		arr[i][0] = arr[0][i]
-//	}
-//	// 逐行填表
-//	for i := 1; i < len(arr); i++ {
-//		for j := 1; j < len(arr[0]); j++ {
-//			arr[i][j] = f(house[i], house[j]) + arr[i][j-1]
-//		}
-//		if arr[i][len(arr[0])-1] > arr[i][len(arr[0])-1] {
-//			arr[i][len(arr[0])-1] = arr[i][len(arr[0])-1]
-//		}
-//	}
-//	fmt.Println(arr[len(arr)-1][len(arr[0])-1])
-//}
-//
-//func f(x, y int) int {
-//	if x > y {
-//		return x - y
-//	}
-//	return y - x
-//}
-
-/*
-a:=0
-	b:=0
-	for {
-		n, _ := fmt.Scan(&a,&b)
-		if n == 0 {
-			break
-		} else {
-			fmt.Printf("%d\n",a+b)
-		}
-	}
- */
-
-/*
-n:=0
-    ans:=0
-
-    fmt.Scan(&n)
-    for i := 0; i < n; i++ {
-        for j := 0; j < n; j++ {
-            x:=0
-            fmt.Scan(&x)
-            ans = ans + x
-        }
-    }
-    fmt.Printf("%d\n",ans)
- */
--- structure/stack.go
+++ structure/stack.go
@@ -2,42 +2,101 @@ package structure
 
 import "errors"
 
-type Stack []interface{}
+/*
+ 顺序栈
+ */
 
-func CreateStack() *Stack {
-	var s = Stack{}
-	return &s
+type ArrStack struct {
+	data []int
+	//cap int	// 容量
 }
 
-func (stack Stack) Len() int {
-	return len(stack)
+func NewArrStack() ArrStack {
+	s := ArrStack{[]int{}}
+	return s
 }
 
-func (stack Stack) Cap() int {
-	return cap(stack)
+func (stack *ArrStack) Len() int {
+	return len(stack.data)
 }
 
-func (stack *Stack) Push(value interface{}) {
-	*stack = append(*stack, value)
+func (stack *ArrStack) Push(value int) {
+	stack.data = append(stack.data, value)
 }
 
-func (stack Stack) Top() (interface{}, error) {
-	if len(stack) == 0 {
-		return nil, errors.New("Out of index, len is 0")
+func (stack *ArrStack) Top() (int, error) {
+	length := len(stack.data)
+	if length == 0 {
+		return 0, errors.New("the Stack is empty")
 	}
-	return stack[len(stack)-1], nil
+	return stack.data[length-1], nil
 }
 
-func (stack *Stack) Pop() (interface{}, error) {
-	theStack := *stack
-	if len(theStack) == 0 {
-		return nil, errors.New("Out of index, len is 0")
+func (stack *ArrStack) Pop() (int, error) {
+	length := len(stack.data)
+	if length == 0 {
+		return 0, errors.New("the Stack is empty")
 	}
-	value := theStack[len(theStack)-1]
-	*stack = theStack[:len(theStack) - 1]
-	return value, nil
+	temp := stack.data[length-1]
+	stack.data = stack.data[:length-1]
+	return temp, nil
 }
 
-func (stack Stack) IsEmpty() bool {
-	return len(stack) == 0
+func (stack *ArrStack) IsEmpty() bool {
+	if len(stack.data) == 0 {
+		return true
+	}
+	return false
+}
+
+/*
+ 链式栈
+ */
+type LinkStack struct {
+	top *StackNode
+	len int
+	//cap int	// 容量
+}
+
+type StackNode struct {
+	value int
+	next *StackNode
+}
+
+func NewLinkStack() LinkStack {
+	return LinkStack{nil, 0}
+}
+
+func (stack *LinkStack) Len() int {
+	return stack.len
+}
+
+func (stack *LinkStack) Push(value int) {
+	node := StackNode{value, stack.top}
+	stack.top = &node
+	stack.len++
+}
+
+func (stack *LinkStack) Top() (int, error) {
+	if stack.len == 0 {
+		return -1, errors.New("the Stack is empty")
+	}
+	return stack.top.value, nil
+}
+
+func (stack *LinkStack) Pop() (int, error) {
+	if stack.len == 0 {
+		return -1, errors.New("the Stack is empty")
+	}
+	temp := stack.top.value
+	stack.top = stack.top.next
+	stack.len--
+	return temp, nil
+}
+
+func (stack *LinkStack) IsEmpty() bool {
+	if stack.len == 0 {
+		return true
+	}
+	return false
 }

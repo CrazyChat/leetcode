0a0c77c9a3a8d5fa6d2b5307223304a6c1933228
MingTaoChen <mail@mittacy.com>
2020-05-09 13:45:42 +0800

    最小栈

--- main.go
+++ main.go
@@ -2,9 +2,60 @@ package main
 
 import (
 	"fmt"
-	"github.com/crazychat/leetcode/stack"
+	"math"
 )
 
 func main() {
-	fmt.Println(stack.CalPoints([]string{"5","-2","4","C","D","9","+","+"}))
+	minStack := MinStack{}
+	minStack.Push(-2)
+	minStack.Push(0)
+	minStack.Push(-3)
+	fmt.Println(minStack.GetMin())
+	minStack.Pop()
+	fmt.Println(minStack.Top())
+	fmt.Println(minStack.GetMin())
 }
+
+type MinStack struct {
+	Val []int
+	Min []int
+}
+
+
+/** initialize your data structure here. */
+func Constructor() MinStack {
+	return MinStack{[]int{}, []int{}}
+}
+
+
+func (this *MinStack) Push(x int)  {
+	this.Val = append(this.Val, x)
+	if x <= this.GetMin() {
+		this.Min = append(this.Min, x)
+	}
+}
+
+
+func (this *MinStack) Pop()  {
+	if this.Val[len(this.Val)-1] == this.GetMin() {
+		this.Min = this.Min[:len(this.Min)-1]
+	}
+	this.Val = this.Val[:len(this.Val)-1]
+}
+
+
+func (this *MinStack) Top() int {
+	return this.Val[len(this.Val)-1]
+}
+
+
+func (this *MinStack) GetMin() int {
+	if len(this.Min) == 0 {
+		return math.MaxInt64
+	}
+	return this.Min[len(this.Min)-1]
+}
+
+
+
+
---
+++ stack/minStack.go
@@ -0,0 +1,63 @@
+package stack
+
+import "math"
+
+/*
+155. 最小栈
+设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+
+push(x) —— 将元素 x 推入栈中。
+pop() —— 删除栈顶的元素。
+top() —— 获取栈顶元素。
+getMin() —— 检索栈中的最小元素。
+ */
+
+/*
+法一：
+type MinStack struct {
+	Val []int
+	Min []int
+}
+ */
+
+// 法二
+type MinStack struct {
+	head *MinStackNode
+}
+
+type MinStackNode struct {
+	val int
+	min int
+	next *MinStackNode
+}
+
+/** initialize your data structure here. */
+func Constructor() MinStack {
+	return MinStack{&MinStackNode{min: math.MaxInt64, next: nil}}
+}
+
+
+func (this *MinStack) Push(x int)  {
+	newNode := MinStackNode{ val: x, next: this.head }
+	if x <= this.head.min {
+		newNode.min = x
+	} else {
+		newNode.min = this.head.min
+	}
+	this.head = &newNode
+}
+
+
+func (this *MinStack) Pop()  {
+	this.head = this.head.next
+}
+
+
+func (this *MinStack) Top() int {
+	return this.head.val
+}
+
+
+func (this *MinStack) GetMin() int {
+	return this.head.min
+}
